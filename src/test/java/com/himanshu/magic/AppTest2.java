02d9f9b2ca0ed20dbc0ec1d77e4928ce945959af
Your Name <you@example.com>
2020-07-10 17:08:19 +0530

    new

--- src/test/java/com/himanshu/magic/AppTest2.java
+++ src/test/java/com/himanshu/magic/AppTest2.java
@@ -9,7 +9,7 @@ import org.junit.jupiter.api.Test;
 class AppTest2 {
 
 	@Test
-	void test() throws FileNotFoundException {
+	void test10() throws FileNotFoundException {
 		App pp1 = new App();
 		int exp = 1;
 		int act=App.result();
@@ -43,6 +43,42 @@ class AppTest2 {
 		int act=App.result();
 		assertEquals(exp, act);
 	}
+	@Test
+	void test5() throws FileNotFoundException {
+		App pp1 = new App();
+		int exp = (Integer) 1/0;
+		int act=App.result();
+		assertEquals(exp, act);
+	}
+	@Test
+	void test6() throws FileNotFoundException {
+		App pp1 = new App();
+		int exp = (Integer) 1/0;
+		int act=App.result();
+		assertEquals(exp, act);
+	}
+	@Test
+	void test7() throws FileNotFoundException {
+		App pp1 = new App();
+		int exp = (Integer) -1;
+		int act=App.result();
+		assertEquals(exp, act);
+	}
+	@Test
+	void test8() throws FileNotFoundException {
+		App pp1 = new App();
+		int exp = (int) 0.00/1;
+		int act=App.result();
+		assertEquals(exp, act);
+	}
+	@Test
+	void test9() throws FileNotFoundException {
+		App pp1 = new App();
+		int exp = (int) Math.ceil(0/0);
+		int act=App.result();
+		assertEquals(exp, act);
+	}
+	
 	
 
 }
